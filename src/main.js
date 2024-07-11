6c12f3565aaac2429926906d711e7f0fbf018956
cytsai-robin <tjy88810@gmail.com>
2024-07-11 17:08:09 +0800

    Fix_index_3

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const mainContent = document.getElementById('main-content');
 
+    const routes = {
+        '/': 'home.html',
+        '/home': 'home.html',
+        '/projects': 'projects.html',
+        '/honors': 'honors.html',
+        '/cv': 'cv.html'
+    };
+
     const loadPage = (page, pushState = true) => {
-        const baseUrl = 'src/pages/';
-        const fullUrl = baseUrl + page + '.html';
+        const fullUrl = 'src/pages/' + routes[page];
 
         fetch(fullUrl)
             .then(response => response.text())
@@ -19,39 +26,33 @@ document.addEventListener('DOMContentLoaded', () => {
             .catch(error => console.error('Error loading page:', error));
     };
 
-    // Load the initial page based on the current URL or default to home
-    const path = location.pathname.split('/').pop();
-    const initialPage = path ? path.replace('.html', '') : 'home';
-    loadPage(initialPage, false);
+    const initialPath = window.location.pathname;
+    loadPage(initialPath, false);
 
-    // Add event listeners to navigation links
     document.querySelectorAll('nav ul li a').forEach(link => {
         link.addEventListener('click', (event) => {
             event.preventDefault();
             document.querySelectorAll('nav ul li a').forEach(link => link.classList.remove('active'));
             link.classList.add('active');
-            const page = link.getAttribute('href').replace('.html', '');
+            const page = link.getAttribute('href');
             loadPage(page);
         });
     });
 
-    // Handle browser back/forward navigation
     window.addEventListener('popstate', (event) => {
         if (event.state && event.state.page) {
             loadPage(event.state.page, false);
         }
     });
 
-    // Menu toggle for small screens
     const menuToggle = document.querySelector('.menu-toggle');
     menuToggle.addEventListener('click', () => {
         menuToggle.classList.toggle('active');
         document.querySelector('nav').classList.toggle('active');
     });
 
-    // Open PDFs in a new tab on mobile
     if (window.innerWidth <= 600) {
-        const cvLink = document.querySelector('nav ul li a[href="cv"]');
+        const cvLink = document.querySelector('nav ul li a[href="/cv"]');
         cvLink.addEventListener('click', (event) => {
             event.preventDefault();
             window.open('src/assets/pdf/CV_resume-cytsai.pdf', '_blank');
